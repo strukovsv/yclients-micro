e22ba17fe7d1a51d998cc6d41b074433e375b04a
Струков Сергей 29162 RU <sstrukov0016@smartds.ru>
2025-02-01 22:45:26 +0700

    Заменил telegram_id на chat_id В сообщениях от бота, оставил только chat_id

--- src/micro/models/bot_events.py
+++ src/micro/models/bot_events.py
@@ -6,6 +6,8 @@ from typing import Any, List, Optional
 from pydantic_avro.base import AvroBase
 from pydantic import Field, BaseModel
 
+import micro.pg_ext as base
+
 from micro.models.header_event import HeaderEvent
 
 logger = logging.getLogger(__name__)
@@ -28,20 +30,25 @@ class MenuItem:
 
 
 class TelegramUser(BaseModel):
-    """Пользователь в telegram"""
+    """Пользователь в telegram message.from_user"""
 
-    id: int  # noqa
-    name: str  # noqa
+    id: str  # noqa
     first_name: str  # noqa
     last_name: str  # noqa
-    telegram_id: str  # noqa
-    telegram_username: str  # noqa
-    client_id: int | None  # noqa
-    staff_id: int | None  # noqa
-    access: List[str] | None  # noqa
-    password: str | None  # noqa
-    phone: str | None  # noqa
-    staff: str | None
+    username: str  # noqa
+    language_code: str  # noqa
+
+    access: list | None = []  # noqa
+
+    def __init__(self, **kwarg):
+        logger.info(f"{kwarg}")
+        super().__init__(
+            id=str(kwarg["id"]),
+            first_name=kwarg["first_name"],
+            last_name=kwarg["last_name"],
+            username=kwarg["username"],
+            language_code=kwarg["language_code"],
+        )
 
     def acc(self, tp_list: any) -> bool:
         """Проверить уровень доступа"""
@@ -50,12 +57,68 @@ class TelegramUser(BaseModel):
                 return True
         return False
 
+    async def fill_access(self) -> list:
+        # Вернуть запись из таблицы
+        for user in await base.fetchall(
+            "select * from passwd p where p.telegram_id = %(id)s",
+            {"id": self.id},
+        ):
+            # Запрос
+            # logger.info(f"get access from: {user}")
+            # Вернуть массив доступа, убрать пробелы
+            self.access = [acc.strip() for acc in user["access"].split(",")]
+
+    async def get_staff(self) -> list:
+        # Вернуть запись из таблицы
+        for user in await base.fetchall(
+            "select * from passwd p where p.telegram_id = %(id)s",
+            {"id": self.id},
+        ):
+            # Запрос
+            # logger.info(f"get access from: {user}")
+            # Вернуть массив доступа, убрать пробелы
+            return user["first_name"]
+        return None
+
+    async def fill(self) -> list:
+        """Найти в таблице пользователя, если нет создать"""
+        if (
+            await base.fetchone(
+                "select * from passwd p where p.telegram_id = %(id)s",
+                {"id": self.id},
+            )
+            is None
+        ):
+            logger.info(f"new telegram user: {self.username}")
+            # Запомнить нового пользователя в таблице
+            await base.execute(
+                """
+        insert into passwd
+        (name, first_name, last_name, telegram_id, telegram_username, access)
+        values
+        (%(username)s, %(first_name)s, %(last_name)s, %(id)s, %(username)s, 'user')
+        """,
+                self.__dict__,
+            )
+        # Заполнить уровни доступа
+        await self.fill_access()
+
+    async def get_chat_id_for_access(access: str) -> list:
+        result = await base.fetchall(
+            f"""
+    select p.telegram_id
+    from passwd p
+    where concat(',', lower(p.access), ',') like '%,_{access.strip().lower()},%'""",
+        )
+        return [int(user["telegram_id"]) for user in result]
+
 
 class BotBaseClass(HeaderEvent):
     user: TelegramUser  # noqa
+    chat_id: str
 
     def route_key(self):
-        return self.user.telegram_id
+        return self.chat_id
 
 
 class BotEnteredTextMessage(BotBaseClass):
@@ -131,12 +194,12 @@ class EventMenuItem(BaseModel):
 class BotSendBase(HeaderEvent):
     """Послать боту меню"""
 
-    telegram_id: int = Field(
-        ..., description="Идентификатор user в telegram"
+    chat_id: int = Field(
+        ..., description="Идентификатор чата в telegram"
     )  # noqa
 
     def route_key(self):
-        return self.telegram_id
+        return self.chat_id
 
 
 class BotSendCalendar(BotSendBase):
@@ -158,7 +221,7 @@ class BotSendMenu(BotSendBase):
     # fmt: on
 
     async def send_menu(
-        telegram_id: int,
+        chat_id: int,
         items: list,
         query: str = None,
         row_width: int = None,
@@ -168,7 +231,7 @@ class BotSendMenu(BotSendBase):
     ):
         """Отправить сообщение на создание меню в боте
 
-        :param int telegram_id: идентификтор пользователя
+        :param int chat_id: идентификтор чата
         :param list items: список элементов меню (title, callback_data)
         :param str query: Текст запроса меню, defaults to None
         :param int row_width: Кол-во элементов в строке, defaults to None
@@ -177,7 +240,7 @@ class BotSendMenu(BotSendBase):
         # Создать сообщение отправить меню
         await BotSendMenu(
             # идентификтор получателя в telegram
-            telegram_id=telegram_id,
+            chat_id=chat_id,
             # Элементы меню
             items=[
                 EventMenuItem(
@@ -186,7 +249,7 @@ class BotSendMenu(BotSendBase):
                         (
                             f"{prefix}.{item.callback_data}"
                             if prefix
-                            else f'{item.callback_data}{item.callback_data2}'
+                            else f"{item.callback_data}{item.callback_data2}"
                         )
                         # + f".{item.callback_data2}"
                         # if item.callback_data2
@@ -210,7 +273,7 @@ class BotSendMenu(BotSendBase):
         row_width: int = 3,
     ):
         await BotSendMenu.send_menu(
-            telegram_id=obj.user.telegram_id,
+            chat_id=obj.chat_id,
             items=items,
             chain_uuid=obj.header.chain_uuid,
             row_width=row_width,
@@ -231,7 +294,7 @@ class BotSendMenu(BotSendBase):
         for id, name in elements:
             items.append(MenuItem(name, str(id)))
         await BotSendMenu.send_menu(
-            telegram_id=obj.user.telegram_id,
+            chat_id=obj.chat_id,
             items=items,
             chain_uuid=obj.header.chain_uuid,
             row_width=row_width,
@@ -247,21 +310,3 @@ class BotSendText(BotSendBase):
     text: str | List[str] = Field(
         None, description="Текс сообщения или массив сообщений"
     )  # noqa
-
-    # def __init__(
-    #     telegram_id: int,
-    #     text: str = None,
-    # ):
-    #     """Отправить сообщение в бот
-
-    #     :param int telegram_id: идентификтор пользователя
-    #     :param str text: Текст сообщения
-    #     """
-    #     self.t
-    #     # Послать сообщение сообщение отправить меню
-    #     await BotSendText(
-    #         # идентификтор получателя в telegram
-    #         telegram_id=telegram_id,
-    #         # Текст сообщения
-    #         text=text,
-    #     ).send()
