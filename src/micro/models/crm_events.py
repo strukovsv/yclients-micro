e674be2c4eb2044bfd164b449c278692f44d9ee1
Струков Сергей 29162 RU <sstrukov0016@smartds.ru>
2026-05-26 20:24:34 +0700

    Добавил событие в CRM создать задачу

--- src/micro/models/crm_events.py
+++ src/micro/models/crm_events.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
-
 from pydantic import Field
-
+from datetime import date
 from micro.models.header_event import HeaderEvent
 
 
@@ -31,6 +30,23 @@ class ClientStatusChanged(HeaderEvent):
     client_id: int = Field(..., description="Идентификатор клиента",)  # noqa
     from_state: str | None = Field(None, description="Переход из статуса клиента",)  # noqa
     to_state: str = Field(..., description="Переход в статус клиента",)  # noqa
+    # fmt: on
+
+    def route_key(self):
+        return self.client_id
+
+
+class ClientTaskCreationRequested(HeaderEvent):
+    """Создать задачу в CRM"""
+
+    # fmt: off
+    caption: str = Field(..., description="Название",)  # noqa
+    status: str | None = Field(None, description="Статус задачи",)  # noqa
+    start_date: date | None = Field(None, description="Дата начала задачи",)  # noqa
+    end_date: date | None = Field(None, description="Дата завершения задачи",)  # noqa
+    description: str = Field(..., description="Описание задачи",)  # noqa
+    client_id: int = Field(..., description="Идентификатор клиента",)  # noqa
+    priority: str | None = Field(None, description="Приоритет задачи",)  # noqa
     # fmt: on
 
     def route_key(self):
